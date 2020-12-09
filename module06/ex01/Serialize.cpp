d61c25fe156433446abec9fe989b7b8b0d83af3e
Sequoiageant <j.nolleau88@gmail.com>
2020-12-09 15:36:48 +0100

    some cleaning

--- module06/ex01/Serialize.cpp
+++ module06/ex01/Serialize.cpp
@@ -6,16 +6,15 @@
 /*   By: julnolle <julnolle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 15:46:59 by julnolle          #+#    #+#             */
-/*   Updated: 2020/12/09 13:05:19 by julnolle         ###   ########.fr       */
+/*   Updated: 2020/12/09 15:35:23 by julnolle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
-#include <sstream>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
-#include <cstring>
+
 struct Data
 {
 	std::string	s1;
@@ -23,14 +22,6 @@ struct Data
 	std::string s2;
 };
 
-/*int	ft_strlen(char *str)
-{
-	int len = 0;
-	while(str[len])
-		len++;
-	return (len);
-}
-*/
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	if (dst == NULL && src == NULL)
@@ -52,18 +43,7 @@ char	*getRandomStr(int len)
 	str[len] = '\0';
 	return (str);
 }
-/*std::string	getRandomStr(int len)
-{
-	std::string str;
-	const std::string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-	for (int i = 0; i < len; ++i)
-	{
-		str += alphabet[rand() % 62];
-	}
-	return (str);
-}
-*/
 void *serialize(void)
 {
 	srand (time(NULL));
@@ -71,9 +51,10 @@ void *serialize(void)
 	char *s2 = getRandomStr(8);
 	int	i = rand() % 200;
 
+	std::cout << "Before serialization:" << std::endl;
 	std::cout << "s1: " << s1 << std::endl;
 	std::cout << "s2: " << s2 << std::endl;
-	std::cout << "i: " << i << std::endl;
+	std::cout << "i: " << i << std::endl << std::endl;
 
 	char *dst = new char[8 + sizeof(int) + 8];
 	char *tmp = dst;
@@ -88,30 +69,11 @@ void *serialize(void)
 	delete [] s1;
 	delete [] s2;
 	return (p);
-}/*
-void *serialize(void)
-{
-	srand (time(NULL));
-	std::stringstream ss;
-	std::string s1 = getRandomStr(8);
-	std::string s2 = getRandomStr(8);
-	int	a = rand() % 200;
-
-	ss << s1 << a << s2;
-	
-	int len = ss.str().length();
-	char *str = new char[len + 1];
-	str[len] = '\0';
-	ss >> str;
-
-	void *p = str;
-	return (p);
-}*/
+}
 
 Data * deserialize(void * raw)
 {
 	Data *data = new Data;
-	// std::stringstream ss;
 
 	char *str = reinterpret_cast<char *>(raw);
 
@@ -120,10 +82,6 @@ Data * deserialize(void * raw)
 
 	int* i = reinterpret_cast<int *>(str + 8);
 	data->n = *i;
-	
-	// std::string tmp(str + 8, (len - 16));
-	// ss << tmp;
-	// ss >> data->n;
 
 	delete [] str;
 	return (data);
@@ -136,6 +94,7 @@ int main(void)
 	void *p = serialize();
 	data 	= deserialize(p);
 
+	std::cout << "After serialization:" << std::endl;
 	std::cout << "data->s1: " << data->s1 << std::endl;
 	std::cout << "data->s2: " << data->s2 << std::endl;
 	std::cout << "data->n: " << data->n << std::endl;
