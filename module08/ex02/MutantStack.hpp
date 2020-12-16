24a2da3c98b0fd1da90082e6140c38e85e58020f
Sequoiageant <j.nolleau88@gmail.com>
2020-12-16 16:05:11 +0100

    begin() & end() made, list behavior checked

---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julnolle <julnolle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/15 12:02:50 by julnolle          #+#    #+#             */
+/*   Updated: 2020/12/16 15:43:37 by julnolle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <string>
+# include <iostream>
+# include <list>
+# include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+
+public:
+	MutantStack(void) : std::stack<T>() {}
+	MutantStack(MutantStack const & copy) : std::stack<T>(copy) {}
+	~MutantStack(void) {}
+	MutantStack& operator=(MutantStack const & rhs) {
+		*this = rhs;
+		return (*this);
+	}
+
+/*	bool			empty() const;
+	unsigned int	size() const;
+	T&				top();
+	const T&		top() const;
+	void 			push(const T& val);
+	void			pop();
+*/
+
+	
+	class iterator : public std::iterator<std::input_iterator_tag, T> {
+
+	private:
+		T *p;
+
+	public:
+		iterator(void);
+		iterator(T* x) : p(x) {}
+		iterator(const iterator& copy) : p(copy.p) {}
+		iterator& operator++() {--p;return *this;}
+		iterator operator++(int) {iterator tmp(*this); operator++(); return tmp;}
+		iterator& operator--() {++p;return *this;}
+		iterator operator--(int) {iterator tmp(*this); operator--(); return tmp;}
+		bool operator==(const iterator& rhs) const {return p==rhs.p;}
+		bool operator!=(const iterator& rhs) const {return p!=rhs.p;}
+		int& operator*() {return *p;}
+		~iterator(void) {}
+		iterator& operator=(iterator const & rhs) {
+			*this = rhs;
+			return (*this);
+		}
+
+	};
+	
+	iterator begin(void) { return iterator(&this->top()); }
+	iterator end(void) { return iterator((&this->top() - this->size())); }
+	
+};
+
+template<typename T>
+std::ostream & operator<<(std::ostream & o, MutantStack<T> const & rhs);
+
+#endif // MUTANTSTACK_HPP
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,41 +6,88 @@
 /*   By: julnolle <julnolle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 12:02:50 by julnolle          #+#    #+#             */
-/*   Updated: 2020/12/15 14:48:15 by julnolle         ###   ########.fr       */
+/*   Updated: 2020/12/16 16:02:27 by julnolle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "Mutantstack.hpp"
+#include "MutantStack.hpp"
 #include <algorithm>
-#include <vector>
 #include <stack>
 
 int main()
 {
-	std::stack<int> s(5);
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "mstack.top(): " << mstack.top() << std::endl;
+
+	std::cout << "mstack.size() before pop: " << mstack.size() << std::endl;
+	mstack.pop();
+	std::cout << "mstack.size() after pop: " << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	mstack.push(41);
+
+	std::cout << "mstack.top() before ++: " << mstack.top()++ << std::endl;
+	std::cout << "mstack.top() after ++: " << mstack.top() << std::endl;
+
+	std::cout << std::endl << "mstack display with iterators:" << std::endl;
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	std::cout << std::endl << "stack copied from mstack:" << std::endl;
+	std::stack<int> s(mstack);
+	std::cout << "s.top(): " << s.top() << std::endl;
+
+	MutantStack<int> mstack2;
+	mstack2.push(42);
+	std::cout << std::endl << std::boolalpha << "is mstack2 empty: " << mstack2.empty() << std::endl;
+	std::cout << "mstack2.size() before pop: " << mstack2.size() << std::endl;
+	mstack2.pop();
+	std::cout << std::endl << std::boolalpha << "is mstack2 empty: " << mstack2.empty() << std::endl;
+	std::cout << "mstack2.size() before pop: " << mstack2.size() << std::endl;
 
 
-// 	MutantStack<int>
-// 	mstack.push(5);
-// 	mstack.push(17);
-// 	mstack;
-// 	std::cout << mstack.top() << std::endl;
-// 	mstack.pop();
-// 	std::cout << mstack.size() << std::endl;
-// 	mstack.push(3);
-// 	mstack.push(5);
-// 	mstack.push(737);
-// //[...]
-// 	mstack.push(0);
-// 	MutantStack<int>::iterator it = mstack.begin();
-// 	MutantStack<int>::iterator ite = mstack.end();
-// 	++it;
-// 	--it;
-// 	while (it != ite)
-// 	{
-// 		std::cout << *it << std::endl;
-// 		++it;
-// 	}
-// 	std::stack<int> s(mstack);
+
+	std::cout << std::endl << "------list same as mstack: ------" << std::endl;
+	std::list<int> lst;
+	lst.push_front(5);
+	lst.push_front(17);
+	std::cout << "lst.front(): " << lst.front() << std::endl;
+
+	std::cout << "lst.size() before pop: " << lst.size() << std::endl;
+	lst.pop_front();
+	std::cout << "lst.size() after pop: " << lst.size() << std::endl;
+
+	lst.push_front(3);
+	lst.push_front(5);
+	lst.push_front(737);
+	lst.push_front(0);
+	lst.push_front(41);
+
+	std::cout << "lst.front() before ++: " << lst.front()++ << std::endl;
+
+	std::cout << std::endl << "lst display with iterators:" << std::endl;
+	std::list<int>::iterator first = lst.begin();
+	std::list<int>::iterator last = lst.end();
+	++it;
+	--it;
+	while (first != last)
+	{
+		std::cout << *first << std::endl;
+		++first;
+	}
+
+
 	return 0;
 }
