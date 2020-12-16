4f36c398122813245d3365f0703beb3a6d6aa78d
Sequoiageant <j.nolleau88@gmail.com>
2020-12-16 09:27:37 +0100

    first commit: class

---
+++ module08/ex02/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julnolle <julnolle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/15 12:02:50 by julnolle          #+#    #+#             */
+/*   Updated: 2020/12/15 14:48:15 by julnolle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "Mutantstack.hpp"
+#include <algorithm>
+#include <vector>
+#include <stack>
+
+int main()
+{
+	std::stack<int> s(5);
+
+
+// 	MutantStack<int>
+// 	mstack.push(5);
+// 	mstack.push(17);
+// 	mstack;
+// 	std::cout << mstack.top() << std::endl;
+// 	mstack.pop();
+// 	std::cout << mstack.size() << std::endl;
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// //[...]
+// 	mstack.push(0);
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	std::stack<int> s(mstack);
+	return 0;
+}
